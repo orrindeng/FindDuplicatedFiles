6ffa5c95a1b0c1e96763365dde8404939b7294bc
Deng Aolin <orrin.deng@grapecity.com>
2023-02-06 18:23:11 +0800

    Init code

---
+++ FindDuplicatedFiles/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace FindDuplicatedFiles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Find_Click(object sender, RoutedEventArgs e)
+        {
+            var name = NameTextBox.Text;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            string repositoryPath = name;
+            if (!name.Contains(":"))
+            {
+                repositoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ForguncyCollaboration", name);
+            }
+            
+            if (!Directory.Exists(repositoryPath))
+            {
+                ResultTextBox.AppendText($"目录 {repositoryPath} 不存在。");
+                return;
+            }
+
+            var subFolders = Directory.EnumerateDirectories(repositoryPath);
+
+            foreach (var subFolder in subFolders)
+            {
+                Task.Run(() =>
+                {
+                    var files = Directory.EnumerateFiles(subFolder, "*", SearchOption.AllDirectories);
+
+                    var grouping = files.GroupBy(f => Path.GetFileName(f));
+
+                    foreach (var g in grouping)
+                    {
+                        if (g.Count() > 1)
+                        {
+                            var appendText = string.Join(Environment.NewLine, g);
+                            
+                            if (!string.IsNullOrEmpty(appendText))
+                            {
+                                Dispatcher.Invoke(() =>
+                                {
+                                    ResultTextBox.AppendText($"{Environment.NewLine}----------{Environment.NewLine}");
+                                    ResultTextBox.AppendText(appendText);
+                                });
+                            }
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
